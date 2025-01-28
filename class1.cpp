998cd00018ad1a7d480bc0a19654f2cf40d9a955
Zunayed <mdzunaed4@gmail.com>
2025-01-28 23:25:59 +0600

    using std

--- class1.cpp
+++ class1.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
+using namespace std;
 
 int main(){
 
-    std::cout<<"Hello World";
+    //std::cout<<"Hello World"; // without using namespace std
+    std::cout<<"Hello World"; // with using namespace std
     return 0;
 }
